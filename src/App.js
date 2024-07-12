7f0fa650178b336490100c59b9f18bc0d7161f80
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-07-12 16:09:08 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import * as deepar from 'deepar';
 import './App.css';
 
-function App() {
+const DEEP_AR_API_KEY = '5a86ec98a5e159a81ae629abf5fb9450bb07b2f2d3eca83ca332834a564eed0ff21f5fcb59e5050e';
+const effectUrl = 'https://wearifyai.github.io/ARTryOn/?effect=bows'; // Example effect URL
+
+
+
+
+const App = () => {
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    async function initializeDeepAR() {
+      // try {
+        await deepar.initialize({
+          licenseKey: DEEP_AR_API_KEY,
+          previewElement: document.querySelector('#deepar-div'),
+          // effect: 'bows'
+          effect: 'https://cdn.jsdelivr.net/npm/deepar/effects/aviators'
+
+        });
+        setInitialized(true);
+      // } catch (error) {
+      //   console.error('DeepAR initialization error:', error);
+      // }
+    }
+
+    if (!initialized) {
+      initializeDeepAR();
+    }
+  }, [initialized]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='ar-screen' id='deepar-div'></div>
     </div>
   );
-}
+};
 
 export default App;
